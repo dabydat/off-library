9bee10df0384fc837a073e16210733798e323821
dabydat <davidgomezdev@gmail.com>
2025-11-02 22:33:25 -0400

    feat(gateway): added version uri type

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -2,15 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { GatewayModule } from './gateway.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ConfigService } from '@nestjs/config';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe, VersioningType } from '@nestjs/common';
 import { SwaggerModule } from '@nestjs/swagger';
 import { setupSwagger } from './config/swagger';
+import { API_PREFIX, UriVersion } from '@app/common-core/infrastructure/constants/version.constant';
 
 async function bootstrap() {
   const app: NestExpressApplication = await NestFactory.create<NestExpressApplication>(GatewayModule);
   const configService = app.get<ConfigService>(ConfigService);
 
   app.enableCors();
+
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: UriVersion.V1,
+  }).setGlobalPrefix(API_PREFIX);
 
   const port: number | undefined = configService.get<number | undefined>('api.port');
   const serverUrl: string | undefined = configService.get<string | undefined>('api.server');
---
+++ libs/common-core/src/infrastructure/constants/version.constant.ts
@@ -0,0 +1,6 @@
+export const API_PREFIX = 'api';
+
+export enum UriVersion {
+    V1 = '1',
+    V2 = '2',
+}
